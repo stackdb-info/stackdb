9d4682d490f8c01ec9a85b910237e5df0ccc867d
leo <leo@vps462003.ovh.net>
2021-07-27 16:53:32 +0200

    add: populate script upgrade and compose fix

--- populate_db.sh
+++ populate_db.sh
@@ -1,3 +1,3 @@
 cd yml_to_graphql
-SCHEMA_PATH="../db/schema.graphql" GRAPHQL_SERVER="http://localhost:8080" YML_DB_PATH="../db/database" node populate.js
+SCHEMA_PATH="../db/schema.graphql" GRAPHQL_SERVER="http://localhost:8086" YML_DB_PATH="../db/database" node populate.js
 cd ..
